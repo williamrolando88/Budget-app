c305e79104dd960352118476a665a474ff45b19c
williamrolando88 <william.rolando88@hotmail.com>
2022-04-18 18:06:11 -0500

    Fix rendering home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   resources :categories do
     resources :operations
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "pages#splash"
+  
+  root "categories#index"
 end
