ac5cc26bfe122f36ffcfbfbb3a1a9aca2837ccd3
williamrolando88 <william.rolando88@hotmail.com>
2022-04-18 12:34:39 -0500

    Update routing as required

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :operations
   resources :users
+  resources :categories do
+    resources :operations
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
